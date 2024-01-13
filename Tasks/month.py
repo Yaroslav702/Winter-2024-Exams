2c6eab049811a089705ea26c4bea22fc980e28d4
vysotskyi <manypresents34@gmail.com>
2024-01-13 12:37:56 +0200

    Renamed variables and changed function declaration.

---
+++ Tasks/month.py
@@ -0,0 +1,19 @@
+# Get month number
+
+MONTHS = [
+  'jan',
+  'feb',
+  'mar',
+  'apr',
+  'may',
+  'jun',
+  'jul',
+  'aug',
+  'sep',
+  'oct',
+  'nov',
+  'dec'
+]
+
+def get_month_number(month_name: str) -> int:
+    pass
