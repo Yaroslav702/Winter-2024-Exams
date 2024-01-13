9ab3e559316a284f781ca6b6d3753218e0e1ea2b
vysotskyi <manypresents34@gmail.com>
2024-01-13 14:12:27 +0200

    Created additional function for the size calculation.

--- Tasks/size.py
+++ Tasks/size.py
@@ -4,3 +4,17 @@ def size(size: int) -> str:
     return converted_size
 
 
+def calculate_size_from_exp(size: int, exponent: int) -> str:
+    result = ''
+
+    match exponent:
+        case 1:
+            result = str(round(size / 1000)) + ' kb'
+        case 2:
+            result = str(round(size / 1000000)) + ' mb'
+        case 3:
+            result = str(round(size / 1000000000)) + ' gb'
+        case _:
+            result = str(size) + ' byte'
+
+    return result
