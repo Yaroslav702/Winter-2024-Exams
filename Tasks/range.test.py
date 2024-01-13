9a902650efa98d4a6c351db5663dc4ff51a6361c
vysotskyi <manypresents34@gmail.com>
2024-01-13 12:02:48 +0200

    Created unittests for _range functions.

---
+++ Tasks/range.test.py
@@ -0,0 +1,32 @@
+import unittest
+from range import _range
+
+class TestRangeFunction(unittest.TestCase):
+
+    def test_valid_range(self):
+        Range = (10, 15)
+        result = _range(Range)
+        self.assertEqual(result, [10, 11, 12, 13, 14, 15])
+
+    def test_empty_range(self):
+        Range = (-2, 2)
+        result = _range(Range)
+        self.assertEqual(result, [-2, -1, 0, 1, 2])
+
+    def test_single_value_range(self):
+        Range = (2, -2)
+        result = _range(Range)
+        self.assertEqual(result, [])
+
+    def test_negative_range(self):
+        Range = (2, 2)
+        result = _range(Range)
+        self.assertEqual(result, [2])
+
+    def test_invalid_range(self):
+        Range = (0, 0)
+        result = _range(Range)
+        self.assertEqual(result, [0])
+
+if __name__ == '__main__':
+    unittest.main()
