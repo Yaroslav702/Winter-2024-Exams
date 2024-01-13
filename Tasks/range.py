56ce9828621fd8812250faa7c8f20f473e766e44
vysotskyi <manypresents34@gmail.com>
2024-01-13 12:07:47 +0200

    Implemented main functionality on the _range function.

--- Tasks/range.py
+++ Tasks/range.py
@@ -2,4 +2,7 @@ def _range(Range: tuple) -> list:
     result_range = []
     start_val, stop_val = Range
 
+    if stop_val >= start_val:
+        result_range = list(range(start_val, stop_val + 1))
+
     return result_range
