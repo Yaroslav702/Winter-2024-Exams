82dfbfd6fa66cc7c7d3cb2d9039b6ceaf6e9aae4
vysotskyi <manypresents34@gmail.com>
2024-01-14 00:25:16 +0200

    Created unittests for the get_longest_arr_string function.

---
+++ Tasks/longest.test.py
@@ -0,0 +1,33 @@
+import unittest
+from longest import get_longest_arr_string
+
+class TestLongestStringFunction(unittest.TestCase):
+
+    def test_longest(self):
+        input_array = ['Roma', 'Kiev', 'Beijing', 'Barcelona', 'Omsk']
+        result = get_longest_arr_string(input_array)
+        self.assertEqual(result, 'Barcelona')
+
+    def test_longest(self):
+        input_array = ['Barcelona', 'Roma', 'Kiev', 'Beijing', 'Omsk']
+        result = get_longest_arr_string(input_array)
+        self.assertEqual(result, 'Barcelona')
+
+    def test_longest(self):
+        input_array = ['Roma', 'Kiev', 'Beijing', 'Omsk', 'Barcelona']
+        result = get_longest_arr_string(input_array)
+        self.assertEqual(result, 'Barcelona')
+   
+    def test_longest(self):
+        input_array = ['Roma', 'Kiev', 'Omsk']
+        result = get_longest_arr_string(input_array)
+        self.assertEqual(result, 'Roma')
+
+    def test_longest(self):
+        input_array = ['Roma']
+        result = get_longest_arr_string(input_array)
+        self.assertEqual(result, 'Roma')
+
+
+if __name__ == '__main__':
+    unittest.main()
